480fc8cd81242fd89bd55c6ce5e3e4b76254980c
Butchman2000 <Bdander2000@msn.com>
2025-05-23 17:24:28 -0400

    Create intro_change_control.py
    created the necessary

---
+++ enforcer/intro_change_control.py
@@ -0,0 +1,32 @@
+'''
+Rules for change control mechanics.
+
+To be incorporated into one or more SOPs.
+
+Code mechanics are to be improved as understanding of entry selections are standardized and limited.
+
+Commit comments are to be full sentences, in the tense that represents what has happened, with default as past tense.
+If more than one action has been performed, no matter how small, it must be explained.
+To explain, if a single deletion is made, use 'Deletion {small / large}'
+For example, for small, if a space, or an empty line, or one line, or one line of notes, is deleted.
+All other deletion activity is to be proclaimed as large.
+
+If a single appended or added material is made, 'Addition {small / large}'
+For example, for small, from a single letter or space,
+up to one full line, or one line of notes.
+All other adding activity is to be proclaimed as large.
+
+Then, in one sentence or more, but no more than three sentences, describe the change that was made.
+
+Then, in one sentence, up to three sentences, describe the impact that was made.
+
+Then, in one sentence, up to three sentences, describe the future risk of the change.
+
+This is to be cumulatively analyzed by the role of quality assurance trained person, and subsequent permission
+is to be requested before full incorporation of the code.
+
+Depending on the severity of the impact, alternative documentation is necessary.
+
+Depending on the severity of the risk, alternative documenation is necessary, including a CAPA.
+'''
+

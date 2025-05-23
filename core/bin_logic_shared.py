97e09cd3e5866ec7a051405b8c31d19745a52d88
Butchman2000 <Bdander2000@msn.com>
2025-05-23 16:59:25 -0400

    Update and rename bin_logic_shared to bin_logic_shared.py
    corrected file name extension

---
+++ core/bin_logic_shared.py
@@ -0,0 +1,7 @@
+# Program: bin_logic_shared.py
+# Author: Brian Anderson
+# Origin Date: 23May2025
+# Version: 1.0
+#
+# Purpose:
+#    /Placeholder for the collection of logic that controls bin distribution and management.
